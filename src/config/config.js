e38f954b6100d557b59fa66e8977be98bd64f56d
tubeeebeee <hoangminhtubeee@gmail.com>
2024-02-27 21:04:04 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 // export const baseURL = process.env.REACT_APP_BASE_URL;
-export const baseURL = "https://pets-api.onrender.com";
+export const baseURL = "https://pets-api-hz7u.onrender.com";
 export const pageSize = 4;
 export const configMoney = {
   style: "currency",
