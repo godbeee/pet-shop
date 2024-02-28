823b5858ad42292f8fe1ee6d37a8c6aa737ee8a0
tubeeebeee <hoangminhtubeee@gmail.com>
2024-02-28 10:14:37 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 // export const baseURL = process.env.REACT_APP_BASE_URL;
-export const baseURL = "https://pets-api-hz7u.onrender.com";
+export const baseURL = "https://suave-dull-titanium.glitch.me";
 export const pageSize = 4;
 export const configMoney = {
   style: "currency",
